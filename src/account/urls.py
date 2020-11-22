91fa9a765f54a66e3178176ff62194f5972ee405
KonstantinSelivanov <selivanov.km@mail.ru>
2020-11-22 18:38:16 +0500

    master

--- src/account/urls.py
+++ src/account/urls.py
@@ -8,7 +8,22 @@ urlpatterns = [
     # Previous login view
     # Предыдущая авторизация
     # path('login/', views.user_authorization, name='login'),
+    # User login template
+    # Шаблон для входа пользователя на сайт
     path('login/', auth_views.LoginView.as_view(), name='login'),
+    # Template for user logout from the site
+    # Шаблон для выхода пользователя с сайта
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # Template dashboard
+    # Шаблон рабочего стола
     path('', views.dashboard, name='dashboard'),
+    # User password change template
+    # Шаблон смены пароля пользователя
+    path('password_change/',
+         auth_views.PasswordChangeView.as_view(),
+         name='password_change'),
+    path('password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
+
 ]
