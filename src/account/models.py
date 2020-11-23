ea4346dd846c71be87f98dc3aa7bd1ece37751d7
KonstantinSelivanov <selivanov.km@mail.ru>
2020-11-23 16:08:25 +0500

    master

--- src/account/models.py
+++ src/account/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class ProfileUser(models.Model):
+    """
+    User profile model. In addition to the standard User model fields,
+    the following fields have been added: 'birthday' = 'date_of_birth'
+    and 'photo' = 'photo'.
+
+    Модель профиля пользователя. Помимо стандартный полей модели User
+    добавлены поля: 'день рождения' = 'date_of_birth' и 'фотография' = 'photo'
+    """
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+    date_of_birth = models.DateField(verbose_name='Дата рождения',
+                                     blank=True,
+                                     null=True)
+    photo = models.ImageField(verbose_name='Фото',
+                              upload_to='users/%Y/%m/%d/',
+                              blank=True)
+
+    def __str__(self):
+        return 'Профиль пользователя {}'.format(self.user.username)
